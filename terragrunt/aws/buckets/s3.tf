37f064ba273c1d6ab0d79a3026060c4ede9974ed
Pat Heard <patrick.heard@cds-snc.ca>
2024-10-30 13:45:46 -0400

    feat: Terraform and GitHub workflows initial commit
    Add the Terraform, Terragrunt and GitHub workflows needed to manage
    the data lake infrastructure.
    
    This also includes the initial three S3 buckets that will hold datasets.

---
+++ terragrunt/aws/buckets/s3.tf
@@ -0,0 +1,71 @@
+#
+# Holds exported data before transformation
+#
+module "raw_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v9.6.7"
+  bucket_name       = "cds-data-lake-raw-${var.env}"
+  billing_tag_value = var.billing_tag_value
+
+  logging = {
+    target_bucket = module.log_bucket.s3_bucket_id
+    target_prefix = "raw/"
+  }
+
+  versioning = {
+    enabled = true
+  }
+}
+
+#
+# ETL jobs process the `Raw` bucket and store the transformed data here
+#
+module "transformed_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v9.6.7"
+  bucket_name       = "cds-data-lake-transformed-${var.env}"
+  billing_tag_value = var.billing_tag_value
+
+  logging = {
+    target_bucket = module.log_bucket.s3_bucket_id
+    target_prefix = "transformed/"
+  }
+
+  versioning = {
+    enabled = true
+  }
+}
+
+#
+# Holds enriched data that has been created by combining multiple transformed datasets
+#
+module "curated_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v9.6.7"
+  bucket_name       = "cds-data-lake-curated-${var.env}"
+  billing_tag_value = var.billing_tag_value
+
+  logging = {
+    target_bucket = module.log_bucket.s3_bucket_id
+    target_prefix = "curated/"
+  }
+
+  versioning = {
+    enabled = true
+  }
+}
+
+#
+# Bucket access logs, stored for 30 days
+#
+module "log_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3_log_bucket?ref=v9.6.7"
+  bucket_name       = "cds-data-lake-bucket-logs-${var.env}"
+  versioning_status = "Enabled"
+
+  lifecycle_rule = {
+    "lifecycle_rule" : {
+      "enabled" : "true",
+      "expiration" : { "days" : "30" }
+    }
+  }
+
+  billing_tag_value = var.billing_tag_value
+}
---
+++ terragrunt/env/production/env_vars.hcl
@@ -0,0 +1,5 @@
+inputs = {
+  account_id        = "739275439843"
+  env               = "production"
+  billing_tag_value = "PlatformDataLake"
+}
