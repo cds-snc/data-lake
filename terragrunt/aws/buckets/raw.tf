b6bf076c472aa2738e8cf1caf05857e85493960e
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-07 10:21:30 -0500

    feat: allow Cost and Usage Report writes to Raw
    Update the Raw bucket policy to allow the Cost and Usage data to be
    directly written to the Raw bucket.
    
    Update the module organization to split the buckets into their own TF files.

---
+++ terragrunt/aws/buckets/raw.tf
@@ -0,0 +1,58 @@
+#
+# Holds exported data before transformation
+#
+module "raw_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v9.6.8"
+  bucket_name       = "cds-data-lake-raw-${var.env}"
+  billing_tag_value = var.billing_tag_value
+
+  logging = {
+    target_bucket = module.log_bucket.s3_bucket_id
+    target_prefix = "raw/"
+  }
+
+  lifecycle_rule = [
+    local.lifecycle_remove_noncurrent_versions,
+    local.lifecycle_transition_storage
+  ]
+
+  versioning = {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "raw_bucket" {
+  bucket = module.cost_usage_report.raw_bucket
+  policy = data.aws_iam_policy_document.raw_bucket.json
+}
+
+#
+# There is a 20kb limit on the size of the policy document
+#
+data "aws_iam_policy_document" "raw_bucket" {
+  statement {
+    sid    = "CostAndUsageReport"
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        "bcm-data-exports.amazonaws.com",
+        "billingreports.amazonaws.com"
+      ]
+    }
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::659087519042:role/BillingExtractTags"
+      ]
+    }
+    actions = [
+      "s3:PutObject",
+      "s3:GetBucketPolicy"
+    ]
+    resources = [
+      module.raw_bucket.s3_bucket_arn,
+      "${module.raw_bucket.s3_bucket_arn}/*"
+    ]
+  }
+}
