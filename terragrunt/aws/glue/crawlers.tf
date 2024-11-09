2aaec3b6e9fadba4fad32e35b5ae80fefaf5d1b9
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-09 22:53:04 +0000

    fix: add JSON classifier to account tags crawler
    Add a custom classifier for crawling files that contain a JSON
    array of objects.

--- terragrunt/aws/glue/crawlers.tf
+++ terragrunt/aws/glue/crawlers.tf
@@ -56,6 +56,7 @@ resource "aws_glue_crawler" "operations_aws_production_account_tags" {
   description   = "Classify the AWS Organization account tag extract"
   database_name = aws_glue_catalog_database.operations_aws_production.name
   table_prefix  = "account_tags_"
+  classifiers   = [aws_glue_classifier.json_object_array.name]
 
   role                   = aws_iam_role.glue_crawler.arn
   security_configuration = aws_glue_security_configuration.encryption_at_rest.name
@@ -76,4 +77,13 @@ resource "aws_glue_crawler" "operations_aws_production_account_tags" {
   })
 
   schedule = "cron(00 13 * * ? *)" # Pickup new accounts each day
+}
+
+# JSON classifier for arrays of objects
+resource "aws_glue_classifier" "json_object_array" {
+  name = "json_object_array"
+
+  json_classifier {
+    json_path = "$[*]"
+  }
 }
