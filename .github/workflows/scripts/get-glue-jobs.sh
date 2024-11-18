74a13114b43db4d5a7be301cd1927b60eac95b29
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-18 11:52:05 -0500

    feat: glue job sync workflow (#28)
    Add a GitHub workflow that retrieves the AWS Glue jobs and
    checks if there are any new or changed jobs.  If there are, a PR is
    opened with the changes.
    
    Once this is tested and working, it will be set to run on a schedule
    so that we have a backup of the visual Glue jobs, which currently
    cannot be managed using Terraform.

---
+++ .github/workflows/scripts/get-glue-jobs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+
+#
+# Retrieves all the AWS glue jobs and creates a directory structure
+# based on the job name. The job details are saved in a JSON file.
+#
+# It is expected that the job name will be in the following format:
+# Business Unit / Product / Environment / Job Name
+#
+
+aws glue get-jobs --query 'Jobs[].Name' --output text |
+while IFS= read -r name; do
+  # Split the name on '/'
+  IFS='/' read -ra segments <<< "$name"
+
+  dir_path=""
+  filename=""
+
+  num_segments=${#segments[@]}
+  for ((i=0; i<num_segments; i++)); do
+    segment="${segments[i]}"
+    segment=$(echo "$segment" | xargs)
+    segment=$(echo "$segment" | sed 's/ /-/g' | tr -cd '[:alnum:]-' | tr '[:upper:]' '[:lower:]')
+
+    if [ "$i" -eq $((num_segments-1)) ]; then
+      filename="$segment.json"
+    else
+      dir_path="$dir_path/$segment"
+    fi
+  done
+
+  dir_path="./terragrunt/aws/glue/etl${dir_path}"
+
+  mkdir -p "$dir_path"
+  aws glue get-job --job-name "$name" --output json > "$dir_path/$filename"
+
+  echo "Created file: $dir_path/$filename"
+done
---
+++ .github/workflows/scripts/sync-glue-jobs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -euo pipefail
+
+#
+# Checks if there are changes to the Glue ETL jobs and creates a PR if yes.
+# This script will update an existing PR if the branch already exists.
+#
+
+JOB_DIR="./terragrunt/aws/glue/etl"
+REMOTE_REPO="origin"
+BRANCH_NAME="chore/glue-job-sync"
+BASE_BRANCH="main"
+PR_TITLE="chore: automated glue job sync"
+PR_BODY="## Summary
+Automated sync of AWS Glue ETL jobs."
+
+# Check for changes in the repository
+if git diff-index --quiet HEAD -- "$JOB_DIR"; then
+    echo "No changes detected."
+    exit 0
+else
+    echo "Changes detected."
+fi
+
+# Check if the remote branch exists
+git fetch "$REMOTE_REPO"
+if git ls-remote --heads "$REMOTE_REPO" "$BRANCH_NAME" | grep -q "$BRANCH_NAME"; then
+    echo "Branch '$BRANCH_NAME' exists. Checking out and updating."
+    git checkout "$BRANCH_NAME"
+else
+    echo "Branch '$BRANCH_NAME' does not exist. Creating new branch."
+    git checkout -b "$BRANCH_NAME" "$BASE_BRANCH"
+fi
+
+# Add changes and commit
+git config user.email "github-actions[bot]@users.noreply.github.com"
+git config user.name "github-actions[bot]"
+git add "$JOB_DIR"
+git commit -m "$PR_TITLE"
+
+# Push branch and create the PR
+git push "$REMOTE_REPO" "$BRANCH_NAME"
+gh pr create --base "$BASE_BRANCH" --head "$BRANCH_NAME" --title "$PR_TITLE" --body "$PR_BODY"
