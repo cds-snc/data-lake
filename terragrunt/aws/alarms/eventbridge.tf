29540c5cb2800d1a48ac48c3adc5392f317bdbd5
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-20 10:37:39 -0500

    fix: Glue Job failure alarm (#37)
    Add a CloudWatch alarm that triggers when the EventBridge rule
    monitoring for failed Glue jobs is invoked.

--- terragrunt/aws/alarms/eventbridge.tf
+++ terragrunt/aws/alarms/eventbridge.tf
@@ -11,31 +11,3 @@ resource "aws_cloudwatch_event_rule" "glue_job_failure" {
   })
 }
 
-#
-# Publish Glue Job failures to SNS using a CloudWatch Alarm message payload.
-# This allows us to use the existing SRE Bot webhooks to post to Slack.
-#
-resource "aws_cloudwatch_event_target" "glue_job_failure" {
-  rule      = aws_cloudwatch_event_rule.glue_job_failure.name
-  target_id = "send-to-sns"
-  arn       = aws_sns_topic.cloudwatch_alarm_action.arn
-
-  input_transformer {
-    input_paths = {
-      jobName = "$.detail.jobName"
-      state   = "$.detail.state"
-      message = "$.detail.message"
-    }
-    input_template = jsonencode({
-      Message = jsonencode({
-        AlarmArn         = "arn:aws:cloudwatch:${var.region}:${var.account_id}:alarm:glue-job-failure",
-        AlarmName        = "glue-job-failure",
-        AlarmDescription = "`<state>` detected for Glue job `<jobName>`",
-        AWSAccountId     = var.account_id,
-        OldStateValue    = "OK",
-        NewStateValue    = "ALARM",
-        NewStateReason   = "<message>",
-      })
-    })
-  }
-}
