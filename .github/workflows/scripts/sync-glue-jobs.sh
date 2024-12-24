ee9c299f3cd0fb6271e69132039441cf1b35d0df
Pat Heard <patrick.heard@cds-snc.ca>
2024-12-24 11:11:03 -0500

    fix: retrieve glue jobs after branch checkout (#68)
    Update the Glue job sync workflow to retrieve the latest
    jobs after the update branch has been checked out.

--- .github/workflows/scripts/sync-glue-jobs.sh
+++ .github/workflows/scripts/sync-glue-jobs.sh
@@ -17,16 +17,17 @@ Automated sync of AWS Glue ETL jobs."
 
 # Check if the remote branch exists
 if git ls-remote --heads "$REMOTE_REPO" "$BRANCH_NAME" | grep -q "$BRANCH_NAME"; then
-    echo "Branch '$BRANCH_NAME' exists. Checking out and updating."
-    git stash
+    echo "Branch '$BRANCH_NAME' exists. Checking out."
     git checkout "$BRANCH_NAME"
-    git stash apply --quiet || true
-    git checkout --theirs -- .
     CREATE_PR="false"
 else
     echo "Branch '$BRANCH_NAME' does not exist. Creating new branch."
-    git checkout -b "$BRANCH_NAME" "$BASE_BRANCH"
+    git fetch "$REMOTE_REPO" "$BASE_BRANCH"
+    git checkout -b "$BRANCH_NAME" "$REMOTE_REPO/$BASE_BRANCH"
 fi
+
+echo "Syncing Glue jobs..."
+.github/workflows/scripts/get-glue-jobs.sh
 
 # Check for changes in the branch
 if git diff-index --quiet HEAD -- "$JOB_DIR"; then
@@ -42,7 +43,7 @@ echo "Committing changes..."
 FILES_CHANGED="$(git status --porcelain | awk '{print $2}')"
 for FILE in $FILES_CHANGED; do
     MESSAGE="chore: regenerate $(basename "$FILE") for $(date -u '+%Y-%m-%d')"
-    SHA="$(git rev-parse $BRANCH_NAME:"$FILE" || "")"
+    SHA="$(git rev-parse $BRANCH_NAME:"$FILE" || echo "")"
     gh api --method PUT /repos/cds-snc/data-lake/contents/"$FILE" \
         --field message="$MESSAGE" \
         --field content="$(base64 -w 0 "$FILE")" \
