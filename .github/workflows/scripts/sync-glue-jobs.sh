1b73dbeac76132f324ea43420e995ee428270ca2
Pat Heard <patrick.heard@cds-snc.ca>
2024-12-24 09:23:43 -0500

    fix: handle missing SHA

--- .github/workflows/scripts/sync-glue-jobs.sh
+++ .github/workflows/scripts/sync-glue-jobs.sh
@@ -43,7 +43,7 @@ echo "Committing changes..."
 FILES_CHANGED="$(git status --porcelain | awk '{print $2}')"
 for FILE in $FILES_CHANGED; do
     MESSAGE="chore: regenerate $(basename "$FILE") for $(date -u '+%Y-%m-%d')"
-    SHA="$(git rev-parse $BRANCH_NAME:"$FILE" || "")"
+    SHA="$(git rev-parse $BRANCH_NAME:"$FILE" || echo "")"
     gh api --method PUT /repos/cds-snc/data-lake/contents/"$FILE" \
         --field message="$MESSAGE" \
         --field content="$(base64 -w 0 "$FILE")" \
