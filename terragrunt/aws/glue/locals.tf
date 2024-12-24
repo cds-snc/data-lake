066ea5c1bd8ef540cf8c5c418ef4a8efbbb8e3cd
Pat Heard <patrick.heard@cds-snc.ca>
2024-12-24 15:25:30 +0000

    fix: separate Superset role access to Glue databases
    Update the IAM access to the Glue catalog databases so that it is
    split by Superset role.  This will make it easier to grant fine-grained
    access to a database.

--- terragrunt/aws/glue/locals.tf
+++ terragrunt/aws/glue/locals.tf
@@ -1,4 +1,11 @@
 locals {
+  # To grant a Superset IAM role access to a Glue catalog database, the role ARN must end with the database name:
+  # - arn:aws:iam::123456789012:role/SupersetAthenaRead-datatabase_name
+  # The Superset role ARNs are managed by the `superset_iam_role_arns` input variable.
+  glue_catalog_databases = [
+    aws_glue_catalog_database.platform_gc_forms_production,
+    aws_glue_catalog_database.operations_aws_production,
+  ]
   glue_crawler_log_group_name = "/aws-glue/crawlers-role${aws_iam_role.glue_crawler.path}${aws_iam_role.glue_crawler.name}-${aws_glue_security_configuration.encryption_at_rest.name}"
   glue_etl_log_group_name     = "/aws-glue/jobs/${aws_glue_security_configuration.encryption_at_rest.name}-role${aws_iam_role.glue_crawler.path}${aws_iam_role.glue_etl.name}"
 }
