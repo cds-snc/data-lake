a4b4b9e3a5c9f01af3697aef92ec0b8857ad96e5
Pat Heard <patrick.heard@cds-snc.ca>
2025-03-12 12:41:42 -0400

    fix: remove all references to `all` database (#156)
    As part of the removal of the SupersetAthenaRead-all IAM role, also remove
    all references to the `all` database.

--- terragrunt/aws/glue/locals.tf
+++ terragrunt/aws/glue/locals.tf
@@ -3,7 +3,6 @@ locals {
   # - arn:aws:iam::123456789012:role/SupersetAthenaRead-datatabase_name
   # The Superset role ARNs are managed by the `superset_iam_role_arns` input variable.
   glue_catalog_databases = [
-    "all", # special case for an IAM role with access to all databases
     aws_glue_catalog_database.operations_aws_production.name,
     aws_glue_catalog_database.platform_gc_forms_production.name,
     aws_glue_catalog_database.platform_support_production.name,
