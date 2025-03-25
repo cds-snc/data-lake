1a200091f46542c53b188cc7a5fcc0af48ba5787
Pat Heard <patrick.heard@cds-snc.ca>
2025-03-25 18:30:05 +0000

    fix: add timezone to yesterday
    Update the `yesterday` date to include a timezone.  This is required by `awswrangler`
    when reading the S3 objects.

--- terragrunt/aws/glue/etl/platform/gc_forms/process_data.py
+++ terragrunt/aws/glue/etl/platform/gc_forms/process_data.py
@@ -117,7 +117,7 @@ def get_new_data(
         logger.info(
             f"Reading s3://{SOURCE_BUCKET}/{SOURCE_PREFIX}/{path}/ data from S3..."
         )
-        yesterday = pd.Timestamp.today() - pd.Timedelta(days=1)
+        yesterday = pd.Timestamp.today(tz="UTC") - pd.Timedelta(days=1)
         data = wr.s3.read_parquet(
             path=f"s3://{SOURCE_BUCKET}/{SOURCE_PREFIX}/{path}/",
             use_threads=True,
