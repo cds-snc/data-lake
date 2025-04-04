e1473a1d2a664803fd9e87fc8485779509a716dd
Pat Heard <patrick.heard@cds-snc.ca>
2025-04-04 15:34:54 -0400

    feat: add CloudWatch `ERROR` metric alarm for ETL logs (#184)
    Add a new CloudWatch alarm and `ERROR` metric to catch errors logged by
    the ETL jobs.  Currently we only have an alarm when the job's state is reported
    as error.  This will allow us to have finer grained error alarms.

--- terragrunt/aws/glue/outputs.tf
+++ terragrunt/aws/glue/outputs.tf
@@ -2,3 +2,8 @@ output "glue_crawler_log_group_name" {
   description = "The name of the Glue Crawler CloudWatch log group."
   value       = local.glue_crawler_log_group_name
 }
+
+output "glue_etl_log_group_name" {
+  description = "The name of the Glue ETL CloudWatch log group."
+  value       = local.glue_etl_log_group_name
+}
