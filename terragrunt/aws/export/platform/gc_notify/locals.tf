0894b935e14e8335ce4274b84128d7dab1fe8f72
Pat Heard <patrick.heard@cds-snc.ca>
2025-05-01 08:26:35 -0400

    feat: switch to export Notify prod data
    Update the RDS snapshot export to pull from Notify prod.  This will allow
    us to perform a full data import and start validating that the prod data
    export and ETL works properly.
    
    The Notify ETL is also being temporarily disabled until a full initial
    import is performed.

--- terragrunt/aws/export/platform/gc_notify/locals.tf
+++ terragrunt/aws/export/platform/gc_notify/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  gc_notify_env                 = "staging"
+  gc_notify_env                 = "production"
   gc_notify_lambda_name         = "platform-gc-notify-export"
-  gc_notify_rds_export_role_arn = "arn:aws:iam::239043911459:role/NotifyExportToPlatformDataLake"
+  gc_notify_rds_export_role_arn = "arn:aws:iam::296255494825:role/NotifyExportToPlatformDataLake"
 }
