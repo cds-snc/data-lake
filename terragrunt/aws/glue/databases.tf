04f5a718acacd60c108dd0a112bea9a24fe2f9fe
Pat Heard <patrick.heard@cds-snc.ca>
2025-05-13 10:53:39 -0400

    feat: add Staging infrastructure workflows (#247)
    Add the GitHub workflows that will manage the Staging infrastructure when there
    are Terraform changes.
    
    Update the failure notifier workflow so that it properly posts messages for failed Staging
    workflows to the correct ops Slack channel.

--- terragrunt/aws/glue/databases.tf
+++ terragrunt/aws/glue/databases.tf
@@ -1,39 +1,39 @@
 resource "aws_glue_catalog_database" "platform_gc_forms_production" {
-  name        = "platform_gc_forms_production"
+  name        = "platform_gc_forms_${var.env}"
   description = "TRANSFORMED: data source path: /platform/gc-forms/*"
 }
 
 resource "aws_glue_catalog_database" "platform_gc_forms_production_raw" {
-  name        = "platform_gc_forms_production_raw"
+  name        = "platform_gc_forms_${var.env}_raw"
   description = "RAW: data source path: /platform/gc-forms/*"
 }
 
 resource "aws_glue_catalog_database" "platform_gc_notify_production" {
-  name        = "platform_gc_notify_production"
+  name        = "platform_gc_notify_${var.env}"
   description = "TRANSFORMED: data source path: /platform/gc-notify/*"
 }
 
 resource "aws_glue_catalog_database" "platform_support_production" {
-  name        = "platform_support_production"
+  name        = "platform_support_${var.env}"
   description = "TRANSFORMED: data source path: /platform/support/*"
 }
 
 resource "aws_glue_catalog_database" "platform_support_production_raw" {
-  name        = "platform_support_production_raw"
+  name        = "platform_support_${var.env}_raw"
   description = "RAW: data source path: /platform/support/*"
 }
 
 resource "aws_glue_catalog_database" "bes_crm_salesforce_production" {
-  name        = "bes_crm_salesforce_production"
+  name        = "bes_crm_salesforce_${var.env}"
   description = "TRANSFORMED: data source path: /bes/crm/salesforce/*"
 }
 
 resource "aws_glue_catalog_database" "operations_aws_production" {
-  name        = "operations_aws_production"
+  name        = "operations_aws_${var.env}"
   description = "TRANSFORMED: data source path: /operations/aws/*"
 }
 
 resource "aws_glue_catalog_database" "operations_aws_production_raw" {
-  name        = "operations_aws_production_raw"
+  name        = "operations_aws_${var.env}_raw"
   description = "RAW: data source path: /operations/aws/*"
 }
