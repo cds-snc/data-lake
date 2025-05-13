ece077c09fe99df0fdd0fc106564455c397506ec
Pat Heard <patrick.heard@cds-snc.ca>
2025-05-13 13:54:28 -0400

    fix: Terraform issues with Staging env create (#248)
    Update the Terraform with fixes required to fully create the Staging
    infrastructure.  This included the following:
    
    - Correcting the cron expression to include the `?` wildcard for day of the week.
    - Updating the database names to be suffixed with `_production`.  We will need
    to adjust the Superset IAM roles before we can create `_staging` Glue databases.

--- terragrunt/aws/export/platform/gc_notify/locals.tf
+++ terragrunt/aws/export/platform/gc_notify/locals.tf
@@ -1,7 +1,7 @@
 locals {
   is_production = var.env == "production"
 
-  cron_expression               = local.is_production ? "cron(0 4 ? * * *)" : "cron(0 0 1 1 1 1970)" # Daily at 4am UTC for prod, never otherwise
+  cron_expression               = local.is_production ? "cron(0 4 ? * * *)" : "cron(0 0 1 1 ? 1970)" # Daily at 4am UTC for prod, never otherwise
   gc_notify_account_id          = local.is_production ? "296255494825" : "239043911459"
   gc_notify_env                 = var.env
   gc_notify_lambda_name         = "platform-gc-notify-export"
--- terragrunt/env/staging/env_vars.hcl
+++ terragrunt/env/staging/env_vars.hcl
@@ -2,4 +2,10 @@ inputs = {
   account_id        = "454671348950"
   env               = "staging"
   billing_tag_value = "PlatformDataLake"
+
+  superset_iam_role_arns = [
+    "arn:aws:iam::257394494478:role/SupersetAthenaRead-platform_gc_forms_staging",
+    "arn:aws:iam::257394494478:role/SupersetAthenaRead-platform_gc_notify_staging",
+    "arn:aws:iam::257394494478:role/SupersetAthenaRead-platform_support_staging",
+  ]
 }
