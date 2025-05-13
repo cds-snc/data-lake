8e5f4d746c8d172f557f4f4a65a6f40dbd46e395
Pat Heard <patrick.heard@cds-snc.ca>
2025-05-13 20:11:35 +0000

    feat: create anomaly detection alarms for GC Forms
    Add anomaly detection alarms for all nightly ETLs run agains the
    Platform / GC Forms datasets.

--- terragrunt/aws/alarms/locals.tf
+++ terragrunt/aws/alarms/locals.tf
@@ -1,4 +1,35 @@
 locals {
+  anomaly_detection_alarms = [
+    {
+      name               = "platform-gc-forms-etl-submissions-processed-record"
+      namespace          = "data-lake/etl/gc-forms"
+      dataset            = "processed-data/submissions"
+      metric_name        = "ProcessedRecordCount"
+      standard_deviation = 1
+    },
+    {
+      name               = "platform-gc-forms-etl-template-processed-record"
+      namespace          = "data-lake/etl/gc-forms"
+      dataset            = "processed-data/template"
+      metric_name        = "ProcessedRecordCount"
+      standard_deviation = 0.5
+    },
+    {
+      name               = "platform-gc-forms-etl-template-to-user-processed-record"
+      namespace          = "data-lake/etl/gc-forms"
+      dataset            = "processed-data/templateToUser"
+      metric_name        = "ProcessedRecordCount"
+      standard_deviation = 0.5
+    },
+    {
+      name               = "platform-gc-forms-etl-user-processed-record"
+      namespace          = "data-lake/etl/gc-forms"
+      dataset            = "processed-data/user"
+      metric_name        = "ProcessedRecordCount"
+      standard_deviation = 0.5
+    }
+  ]
+
   data_lake_namespace                      = "data-lake"
   glue_crawler_metric_filter_error_pattern = "ERROR"
   glue_crawler_error_metric_name           = "glue-crawler-error"
