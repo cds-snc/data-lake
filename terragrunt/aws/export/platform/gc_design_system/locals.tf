153ca3ab7fffd7c94372218976426ffa061ec97e
YannickB <yannick.bounouar@cds-snc.ca>
2025-08-06 16:53:46 -0400

    renaming

--- terragrunt/aws/export/platform/gc_design_system/locals.tf
+++ terragrunt/aws/export/platform/gc_design_system/locals.tf
@@ -2,6 +2,6 @@ locals {
   is_production = var.env == "production"
 
   cron_expression              = local.is_production ? "cron(0 5 * * ? *)" : "cron(0 0 1 1 ? 1970)" # Daily at 5am UTC for prod, never otherwise
-  gc_design_system_lambda_name = "platform-gc-design-system-export"
+  gc_design_system_lambda_name = "platform-gc-design-system-airtable-export"
   gc_design_system_export_path = "platform/gc-design-system/airtable"
 }
