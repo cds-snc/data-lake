e83a983fa3558e7a15fbe30e97115a816c220d97
YannickB <yannick.bounouar@cds-snc.ca>
2025-11-10 10:10:01 -0500

    fmt

--- terragrunt/aws/export/operations/google_analytics/locals.tf
+++ terragrunt/aws/export/operations/google_analytics/locals.tf
@@ -1,6 +1,6 @@
 locals {
   is_production = var.env == "production"
 
-  cron_expression                  = local.is_production ? "cron(0 6 ? * * *)" : "cron(0 0 1 1 ? 1970)" # Daily at 6am UTC for prod, never otherwise
-  google_analytics_lambda_name     = "operations-google-analytics-export"
+  cron_expression              = local.is_production ? "cron(0 6 ? * * *)" : "cron(0 0 1 1 ? 1970)" # Daily at 6am UTC for prod, never otherwise
+  google_analytics_lambda_name = "operations-google-analytics-export"
 }
