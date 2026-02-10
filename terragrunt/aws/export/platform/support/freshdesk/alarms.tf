b1abba08b21ad68fdf761272f7c78d333346cc83
yannickbounouar-cds <yannick.bounouar@cds-snc.ca>
2026-02-10 10:46:03 -0500

    fix: longer timeout and better alarm name (#560)

--- terragrunt/aws/export/platform/support/freshdesk/alarms.tf
+++ terragrunt/aws/export/platform/support/freshdesk/alarms.tf
@@ -1,10 +1,10 @@
 resource "aws_cloudwatch_log_metric_filter" "platform_support_freshdesk_export" {
-  name           = "${local.freshdesk_lambda_name}-error"
+  name           = "${local.freshdesk_lambda_name}-lambda-error"
   pattern        = "ERROR"
   log_group_name = "/aws/lambda/${local.freshdesk_lambda_name}"
 
   metric_transformation {
-    name          = "${local.freshdesk_lambda_name}-error"
+    name          = "${local.freshdesk_lambda_name}-lambda-error"
     namespace     = "data-lake"
     value         = "1"
     default_value = "0"
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_log_metric_filter" "platform_support_freshdesk_export" 
 }
 
 resource "aws_cloudwatch_metric_alarm" "platform_support_freshdesk_export_error_logged" {
-  alarm_name          = "${local.freshdesk_lambda_name}-error-logged"
+  alarm_name          = "${local.freshdesk_lambda_name}-lambda-error-logged"
   alarm_description   = "Errors logged over 1 minute by the Platform / Support / Freshdesk export."
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
@@ -29,7 +29,7 @@ resource "aws_cloudwatch_metric_alarm" "platform_support_freshdesk_export_error_
 }
 
 resource "aws_cloudwatch_metric_alarm" "platform_support_freshdesk_export_errors" {
-  alarm_name          = "${local.freshdesk_lambda_name}-errors"
+  alarm_name          = "${local.freshdesk_lambda_name}-lambda-errors"
   alarm_description   = "Errors metric over 1 minute by the Platform / Support / Freshdesk export."
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
