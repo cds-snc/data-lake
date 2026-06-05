a33c91a6df14badb9978f2113cd2aa2975755ddb
Pat Heard <patrick.heard@cds-snc.ca>
2026-06-05 13:20:36 -0400

    fix: add team's default tag

--- terragrunt/env/common/provider.tf
+++ terragrunt/env/common/provider.tf
@@ -17,6 +17,19 @@ provider "aws" {
     tags = {
       CostCentre = "PlatformDataLake"
       Terraform  = true
+      ssc_cbrid  = "22DI"
+    }
+  }
+}
+
+provider "aws" {
+  alias               = "core_services"
+  region              = var.region
+  allowed_account_ids = [var.account_id]
+ 
+  default_tags {
+    tags = {
+      ssc_cbrid = "22DH"
     }
   }
 }
